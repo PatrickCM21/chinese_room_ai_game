cf1a04ac0accb01136ec5138f149bb9e383bcc9f
Patrick Crown <patrickcrownmilliss@gmail.com>
2025-09-13 09:23:19 +1000

    fixed comma issue

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const app = express()
 
 const cors = require('cors')
 const corsOptions = {
-    origin: ["http://localhost:5173, https://chinese-room-ai-game-client.vercel.app"]
+    origin: ["http://localhost:5173", "https://chinese-room-ai-game-client.vercel.app"]
 }
 
 app.use(express.json())
